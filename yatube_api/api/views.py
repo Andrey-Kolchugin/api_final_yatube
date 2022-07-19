2892c08fa8c425e4b2adde1e430b7d1d178d1e37
Andrey-Kolchugin <a296224@yandex.ru>
2022-07-19 10:05:03 +0300

    0.1

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,19 @@
 # TODO:  Напишите свой вариант
+from posts.models import Post
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from .serializers import PostSerializer
+
+
+# from .permissions import IsAuthorOrReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    #permission_classes = (IsAuthorOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
